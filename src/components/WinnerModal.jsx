6efa9bfed3e68f64e9ffe74715887db51ea232cb
tanushbhootra576 <193266446+tanushbhootra576@users.noreply.github.com>
2025-05-18 22:53:37 +0530

    Update WinnerModal.jsx

--- src/components/WinnerModal.jsx
+++ src/components/WinnerModal.jsx
@@ -1,23 +1,3 @@
-// import React from "react";
-// import PropTypes from "prop-types";
-// import "../index.css"
-// const WinnerModal = ({ winner, onReset }) => {
-//   return (
-//     <div className="modal-overlay">
-//       <div className="modal">
-//         <h2>{winner === "Draw" ? "It's a Draw!" : `${winner} Wins!`}</h2>
-//         <button onClick={onReset}>Restart Game</button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// WinnerModal.propTypes = {
-//   winner: PropTypes.string.isRequired,
-//   onReset: PropTypes.func.isRequired,
-// };
-
-// export default WinnerModal;
 
 import React from "react";
 import PropTypes from "prop-types";
